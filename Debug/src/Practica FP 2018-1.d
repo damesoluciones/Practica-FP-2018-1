6992e2e4cc6eb5b3310d8794175fbf4fb43d8cc3
damesoluciones <cinematropic@gmail.com>
2018-11-04 18:20:55 +0100

    cambio prueba

---
+++ Debug/src/Practica FP 2018-1.d
@@ -0,0 +1,1 @@
+src/Practica\ FP\ 2018-1.d: ../src/Practica\ FP\ 2018-1.cpp
---
+++ src/Practica FP 2018-1.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Practica.cpp
+// Author      : Chuyin y Manuel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Práctica FP 2018-1
+//============================================================================
+
+
+#include <iostream>
+#include <cstdlib>;
+#include <ctime>;
+using namespace std;
+
